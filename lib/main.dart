01d780158cc8a7de5b76c9d06ada0830bac5ed02
prnmshr <h1101211065@student.untan.ac.id>
2024-09-19 08:42:32 +0700

    develop

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Qibla Direction',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: QiblaDirectionPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class QiblaDirectionPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _QiblaDirectionPageState createState() => _QiblaDirectionPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _QiblaDirectionPageState extends State<QiblaDirectionPage> {
+  double _qiblaDirection = 0.0; // Arah kiblat
+  double _currentDirection = 0.0; // Arah magnetometer
+  Position? _currentPosition;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+    magnetometerEvents.listen(_onMagnetometerEvent);
+  }
+
+  // Mendapatkan lokasi pengguna
+  Future<void> _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentPosition = position;
+      _calculateQiblaDirection();
+    });
+  }
+
+  // Menghitung arah kiblat berdasarkan lokasi pengguna
+  void _calculateQiblaDirection() {
+    const double latKabah = 21.4225; // Latitude Ka'bah
+    const double lngKabah = 39.8262; // Longitude Ka'bah
+
+    if (_currentPosition != null) {
+      double userLat = _currentPosition!.latitude;
+      double userLng = _currentPosition!.longitude;
+
+      // Rumus untuk menghitung arah kiblat menggunakan koordinat
+      double qiblaDirection = atan2(
+        sin(_toRadians(lngKabah - userLng)),
+        cos(_toRadians(userLat)) * tan(_toRadians(latKabah)) -
+            sin(_toRadians(userLat)) * cos(_toRadians(lngKabah - userLng)),
+      );
+
+      setState(() {
+        _qiblaDirection = _toDegrees(qiblaDirection);
+        if (_qiblaDirection < 0) {
+          _qiblaDirection += 360;
+        }
+      });
+    }
+  }
+
+  // Konversi derajat ke radian
+  double _toRadians(double degree) {
+    return degree * pi / 180;
+  }
+
+  // Konversi radian ke derajat
+  double _toDegrees(double radian) {
+    return radian * 180 / pi;
+  }
+
+  // Mendapatkan data dari magnetometer untuk arah medan magnet
+  void _onMagnetometerEvent(MagnetometerEvent event) {
+    double x = event.x;
+    double y = event.y;
+    double radians = atan2(y, x);
+    double degrees = _toDegrees(radians);
+    setState(() {
+      _currentDirection = degrees;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Arah Kiblat'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
+            if (_currentPosition != null) ...[
+              Text(
+                'Lokasi Anda: ${_currentPosition!.latitude}, ${_currentPosition!.longitude}',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+            ],
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Arah Kiblat: ${_qiblaDirection.toStringAsFixed(2)}°',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Arah Medan Magnet: ${_currentDirection.toStringAsFixed(2)}°',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 40),
+            Transform.rotate(
+              angle: (_currentDirection - _qiblaDirection) * (pi / 180),
+              child: Icon(
+                Icons.arrow_upward,
+                size: 100,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Putar perangkat ke arah kiblat',
+              style: TextStyle(fontSize: 18),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
